6e8658966c46cdd98333e4680dfdb5cc22f7fdaf
firoj10 <mdfirojhasan.info@gmail.com>
2023-05-02 11:01:53 +0600

    food details added

--- src/components/SingleFood/SingleFood.jsx
+++ src/components/SingleFood/SingleFood.jsx
@@ -1,13 +1,65 @@
 import React from 'react';
+import { Button, Card, CardGroup } from 'react-bootstrap';
 
 const SingleFood = ({food}) => {
-    const {total_view, others_info,details, thumbnail_url}= food;
+    const {phefpicture, chefname,recipe_name, ingredients,experience,
+        Numbersofrecipes,liks,rating,recep_image,published_date,
+        cooking_method,description }= food;
     return (
+       <div>
         <div>
-               <img variant="top" src={thumbnail_url} />
-           <p>{total_view}</p> 
-           <p>{details}</p> 
+            <img src={phefpicture} alt="" />
+            <p>Chef name <span>{chefname}</span></p>
+            <p>Short Bio<span>{description}</span></p>
+            <p>Numbers of Recipes<span>{Numbersofrecipes}</span></p>
+            <p>Years of Experience<span>{experience}</span></p>
+
         </div>
+         <CardGroup>
+        <Card>
+          <Card.Img variant="top" src={recep_image.recepe1} />
+          <Card.Body>
+            <Card.Title>{recipe_name.res1}</Card.Title>
+            <Card.Text>
+            <p>ingredients : <span>{ingredients.ingre1}</span></p>
+            <p>cooking_method : <span>{cooking_method.cok_met1}</span></p>
+            <p>rating: <span>{rating.rat1}</span></p>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+          <Button>Favorite button</Button>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={recep_image.recepe2} />
+          <Card.Body>
+            <Card.Title>{recipe_name.res2}</Card.Title>
+            <Card.Text>
+            <p>ingredients: <span>{ingredients.ingre2}</span></p>
+            <p>cooking_method: <span>{cooking_method.cok_met2}</span></p>
+            <p>rating: <span>{rating.rat1}</span></p>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+          <Button>Favorite button</Button>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={recep_image.recepe3}/>
+          <Card.Body>
+            <Card.Title>{recipe_name.res3}</Card.Title>
+            <Card.Text>
+            <p>ingredients: <span>{ingredients.ingre3}</span></p>
+            <p>cooking_method: <span>{cooking_method.cok_met3}</span></p>
+            <p>rating: <span>{rating.rat1}</span></p>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+          <Button>Favorite button</Button>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+       </div>
     );
 };
 
