cfcfaa969e4e648db2b0d69408f46a6c21bd278e
firoj10 <mdfirojhasan.info@gmail.com>
2023-05-05 11:19:15 +0600

    project last commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,8 +10,8 @@ const Banner = () => {
                     <Col lg={6}>
 <div className='pt-5 mt-5'>
 <h1 className='pt-20 text-success'>Bangladeshi Top Food Vloggers and Influencers</h1> 
-<p className='fw-semibold'>Shef is part of a growing movement to build a more inclusive food system for all. We are proud to support and champion advocacy efforts to expand home cooking laws across the country so that talented cooks everywhere can earn a meaningful income from their own kitchens.
-In the three years since we delivered our very first order, we have now expanded to serve more than 70 million people</p>                  
+<p className='fw-semibold'>Shef is part of a growing movement to build a more inclusive food system for all. We are proud to support and champion advocacy, efforts to expand home cooking laws across the country so that talented cooks everywhere can earn a meaningful income from their own kitchens.
+In the three years since we delivered our very first order, we have now expanded to serve more, than 70 million people</p>                  
 </div>
 </Col>  
 <Col lg={6}>
